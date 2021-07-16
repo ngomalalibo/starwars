0d7e404bbb85ad18d9a5924257b93fa43864beb1
Ngo <ngomalalibo@yahoo.com>
2021-07-16 23:16:31 +0100

    Heroku Deploy

--- src/main/java/com/starwars/movies/controller/MovieController.java
+++ src/main/java/com/starwars/movies/controller/MovieController.java
@@ -6,6 +6,7 @@ import com.starwars.movies.entity.MovieCharacter;
 import com.starwars.movies.repository.CommentRepository;
 import com.starwars.movies.service.MovieCharacterService;
 import com.starwars.movies.service.MovieService;
+import com.starwars.movies.utility.ConvertToInchesAndFeet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,7 +15,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -76,12 +79,24 @@ public class MovieController
      * this endpoint returns the list of characters for a movie. It is able to filter by character list and provides the character height in cm, inches and feet
      */
     @GetMapping("/{id}/characters")
-    public ResponseEntity<Set<MovieCharacter>> getMovieCharacters(@PathVariable String id,
-                                                                  @RequestParam("sortBy") String sortBy,
-                                                                  @RequestParam("direction") String direction,
-                                                                  @RequestParam(value = "gender", required = false) String gender)
+    public ResponseEntity<Object> getMovieCharacters(@PathVariable String id,
+                                                     @RequestParam("sortBy") String sortBy,
+                                                     @RequestParam("direction") String direction,
+                                                     @RequestParam(value = "gender", required = false) String gender)
     {
-        return ResponseEntity.ok(movieService.findMovieCharacters(id, sortBy, direction, gender));
+        
+        Set<MovieCharacter> movieCharacters = movieService.findMovieCharacters(id, sortBy, direction, gender);
+        int filterCriteriaTotalResponse = movieCharacterService.getMovieCharTotalForGender(id, sortBy, direction, gender);
+        int filterCriteriaTotalHeight = movieCharacterService.getMovieCharTotalHeightForGenderCM(id, sortBy, direction, gender);
+        Map<String, Object> result = new HashMap<>()
+        {{
+            put("count", filterCriteriaTotalResponse);
+            put("totalHeight", filterCriteriaTotalHeight);
+            put("totalHeightFeet", ConvertToInchesAndFeet.getMovieCharTotalHeightForGenderFeet(filterCriteriaTotalHeight));
+            put("totalHeightInches", ConvertToInchesAndFeet.getMovieCharTotalHeightForGenderInches(filterCriteriaTotalHeight));
+            put("result", movieCharacters);
+        }};
+        return ResponseEntity.ok(result);
         
     }
     
